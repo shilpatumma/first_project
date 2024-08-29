c0ce5a8d3695f99243bf181779f6a112a6d8728b
shilpatumma <shilpatumma21@gmail.com>
2024-08-29 12:11:35 +0530

    template added

--- first_project/views.py
+++ first_project/views.py
@@ -1,7 +1,15 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
-def hello(request):
-    return HttpResponse("Hello World!")
+# def hello(request):
+#     return HttpResponse("Hello World!")
+
+# def home(request):
+#     return HttpResponse("This is my home")
+
+
+
+
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("This is my home")
+    return render(request, "home.html")
